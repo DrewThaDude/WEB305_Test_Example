df50c1ab04ffd6cd34f8cfa6e5bf08c0fb42901a
studbury <W0726026@myscc.ca>
2022-03-02 05:27:34 -0500

    Update Scaffolded cshtml files

--- RazorPageMovie/Startup.cs
+++ RazorPageMovie/Startup.cs
@@ -14,57 +14,47 @@ namespace RazorPageMovie
 {
     public class Startup
     {
-        public class Startup
-{
-    public Startup(IConfiguration configuration, IWebHostEnvironment env)
-    {
-        Environment = env;
-        Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-    public IWebHostEnvironment Environment { get; }
-
-    public void ConfigureServices(IServiceCollection services)
-    {
-        if (Environment.IsDevelopment())
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
             services.AddDbContext<RazorPagesMovieContext>(options =>
-            options.UseSqlite(
-                Configuration.GetConnectionString("RazorPagesMovieContext")));
-        }
-        else
-        {
-            services.AddDbContext<RazorPagesMovieContext>(options =>
-            options.UseSqlServer(
-                Configuration.GetConnectionString("MovieContext")));
+                    options.UseSqlite(Configuration.GetConnectionString("RazorPagesMovieContext")));
         }
 
-        services.AddRazorPages();
-    }
-
-    public void Configure(IApplicationBuilder app)
-    {
-        if (Environment.IsDevelopment())
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
         }
-        else
-        {
-            app.UseExceptionHandler("/Error");
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapRazorPages();
-        });
-    }
-}
     }
 }
